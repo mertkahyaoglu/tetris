ef39386c7b58d040680d500eb752fc4798746f75
mertkahyaoglu <mertkahyaoglu93@gmail.com>
2017-02-19 11:08:43 +0200

    implement selectors

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -7,15 +7,30 @@ import {
   ROTATE_UP
 } from '../constants/actionTypes';
 
+import {
+  getInitialState,
+  startGame,
+  moveDown,
+  moveLeft,
+  moveRight,
+  rotateDown,
+  rotateUp,
+} from './selectors';
 
-export default function game(state = {}, action) {
+export default function game(state = getInitialState(), action) {
   switch (action.type) {
     case START_GAME:
+      return startGame(state, action);
     case MOVE_DOWN:
+      return moveDown(state, action);
     case MOVE_LEFT:
+        return moveLeft(state, action);
     case MOVE_RIGHT:
+        return moveRight(state, action);
     case ROTATE_DOWN:
+        return rotateDown(state, action);
     case ROTATE_UP:
+        return rotateUp(state, action);
     default:
       return state;
   }
---
+++ src/utils/helpers.js
@@ -0,0 +1,47 @@
+import _ from 'underscore';
+import Tetrominos from '../constants/tetrominos';
+
+// shuffles tetrominos and returns array of info object
+export const shuffleTetrominos = () => {
+  return _.chain(Tetrominos)
+    .keys()
+    .shuffle()
+    .map(key => ({
+      shape: Tetrominos[key].shape,
+      color: Tetrominos[key].color,
+      position: { x: 6, y: 0 }
+    }))
+    .value();
+};
+
+// returns coordinates of cells of a tetromino
+export const getTetrominoCells = (shape, position) => {
+  const cells = [];
+  _.each(shape, (row, i) => {
+    _.each(row, (col, j) => {
+      if (col == 1) {
+        cells.push({
+          x: j + position.x,
+          y: i + position.y
+        });
+      }
+    });
+  });
+  return cells;
+};
+
+export const getCurrentTetrominoCells = (state) => {
+  const { shape, position } = state.tetromino;
+  return getTetrominoCells(shape, position);
+};
+
+export const transpose = (matrix) => {
+  return _.zip.apply(null, matrix).map(row => row.reverse());
+};
+
+export const rtranspose = (matrix) => {
+  const reverse = _.map(matrix, row => row.reverse());
+  return _.map(reverse[0], (col, i) => {
+    return _.map(reverse, (row) => row[i]);
+  });
+};
