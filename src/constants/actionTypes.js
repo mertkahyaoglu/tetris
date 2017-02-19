88b8d5d84f1d2bf9e5b77a2ffe57aa5fca9e767b
mertkahyaoglu <mertkahyaoglu93@gmail.com>
2017-02-19 11:03:11 +0200

    add constants

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,6 @@
+export const START_GAME = 'START_GAME';
+export const MOVE_DOWN = 'MOVE_DOWN';
+export const MOVE_RIGHT = 'MOVE_RIGHT';
+export const MOVE_LEFT = 'MOVE_LEFT';
+export const ROTATE_DOWN = 'ROTATE_DOWN';
+export const ROTATE_UP = 'ROTATE_UP';
